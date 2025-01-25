c02341b90b4423e8f066bb2a79f81bd4c39db84d
garpunk <garpunked@gmail.com>
2025-01-25 16:10:14 -0700

    made the cool pics assignment

---
+++ circleArea.js
@@ -0,0 +1,21 @@
+const PI = 3.14;
+let radius = 3;
+let area = 0;
+area = radius * radius * PI;
+console.log('Area1:', area);
+radius = 4;
+area = radius * radius * PI;
+console.log('Area2:', area);
+
+function circleArea(radius) {
+  const PI = 3.14;
+  let area = 0;
+  function circleArea(radius) {
+    const area = radius * radius * PI;
+  }
+  area = circleArea(3);
+  console.log('Area1:', area);
+
+  area = circleArea(4);
+  console.log('Area2:', area);
+}
